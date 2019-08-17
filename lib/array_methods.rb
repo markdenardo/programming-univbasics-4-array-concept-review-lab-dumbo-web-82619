5a3365cc465046bff6f3838502dd92e92e29950b
Mark DeNardo <mark.denardo@gmail.com>
2019-08-17 21:19:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,6 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array.min
-  return
+  i=0
+  array.length.times {|index|
+   if array[index]<i
+     i=array[index]
+  end
+  }
+  i
 end
