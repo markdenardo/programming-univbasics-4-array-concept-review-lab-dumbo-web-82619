84580a11b15db8aceae1285475270cccbf6dd848
Mark DeNardo <mark.denardo@gmail.com>
2019-08-17 21:05:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,13 @@
 def find_element_index(array, value_to_find)
-  array.length.times
+  array.length.times do |i|
       if array[i] == value_to_find
        return i
     end
-  i += 1
   end
- nil
 end
+nil
+end
+
 def find_max_value(array)
   array.max
   return 
